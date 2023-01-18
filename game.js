d63fcbc0712bb5cc2399a20aa44ba1bf34f69c58
JoaquinArruiz <92789285+JoaquinArruiz@users.noreply.github.com>
2023-01-18 14:30:03 -0300

    added simple round and game functions

--- game.js
+++ game.js
@@ -1,17 +1,48 @@
-let computerOptions = ["rock", "paper", "scissors"];
+let Options = ["rock", "paper", "scissors"];
 let playerOption;
 
-let playerScore = 0
-let computerScore = 0
+let playerScore = 0;
+let computerScore = 0;
+
+
 
 let randomInt = (a) => { // a in case that more options want to be added
-    return Math.floor(Math.random() * a) // default use of a = 3, because the computerOptions array is gonna be [0,1,2]
+    return Math.floor(Math.random() * a); // default use of a = 3, because the computerOptions array is gonna be [0,1,2]
 }
 
+
+
 let getComputerChoice = () => { // takes the randomInt function and returns random str of the computerOptions array
-    let ComputerChoice = computerOptions[randomInt(3)]; 
+    let ComputerChoice = Options[randomInt(3)]; 
     return ComputerChoice;
 }
 
-// playerOption = prompt("Input your option")
 
+
+let  getPlayerChoice = (a) => {
+    let PlayerChoice = Options[a];
+    console.log(PlayerChoice);
+    return PlayerChoice;    
+}
+
+
+let playRound = (a,b) => {
+
+    if (((a == "rock") && (b == "scissors")) || ((a=="paper") && (b=="rock")) || ((a=="scissors") && (b=="paper"))) {
+        playerScore = playerScore + 1
+    }
+    else {
+
+        if (((b == "rock") && (a == "scissors")) || ((b=="paper") && (a=="rock")) || ((b=="scissors") && (a=="paper"))) {
+            computerScore = computerScore + 1
+            return computerScore    
+        }
+    }
+}
+
+let playGame = () => {
+    for (i = 0; i < 5; i++) {
+        getComputerChoice()
+        playRound(PlayerChoice, ComputerChoice)
+    }
+}
