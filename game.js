63cd02e69244b98587c27b8800387fa4e5d77c6f
JoaquinArruiz <92789285+JoaquinArruiz@users.noreply.github.com>
2023-01-24 16:26:03 -0300

    finished project, all working, not knowing issues, needs to improve design/aesthetics

--- game.js
+++ game.js
@@ -1,48 +1,147 @@
-let Options = ["rock", "paper", "scissors"];
-let playerOption;
-
-let playerScore = 0;
-let computerScore = 0;
-
-
-
 let randomInt = (a) => { // a in case that more options want to be added
     return Math.floor(Math.random() * a); // default use of a = 3, because the computerOptions array is gonna be [0,1,2]
 }
 
 
-
 let getComputerChoice = () => { // takes the randomInt function and returns random str of the computerOptions array
-    let ComputerChoice = Options[randomInt(3)]; 
+    let ComputerChoice = Options[randomInt(3)];
+
     return ComputerChoice;
 }
 
 
-
-let  getPlayerChoice = (a) => {
-    let PlayerChoice = Options[a];
-    console.log(PlayerChoice);
-    return PlayerChoice;    
-}
-
-
-let playRound = (a,b) => {
-
-    if (((a == "rock") && (b == "scissors")) || ((a=="paper") && (b=="rock")) || ((a=="scissors") && (b=="paper"))) {
-        playerScore = playerScore + 1
-    }
-    else {
-
-        if (((b == "rock") && (a == "scissors")) || ((b=="paper") && (a=="rock")) || ((b=="scissors") && (a=="paper"))) {
-            computerScore = computerScore + 1
-            return computerScore    
+let playRound = (playerChoice,computerChoice) => {
+    
+    if  (
+        (playerChoice == Options[0] && computerChoice == Options[2]) || 
+        (playerChoice == Options[1] && computerChoice == Options[0]) || 
+        (playerChoice == Options[2] && computerChoice == Options[1])
+        ) 
+        {           
+            playerScore++
+            pointerPMarker[playerScore].innerText = '✅'
+            pointerRoundDesc.innerText = 'PLAYER WINS THE ROUND'
         }
-    }
+
+    if ((computerChoice  == Options[0] && playerChoice == Options[2]) || 
+        (computerChoice == Options[1] && playerChoice == Options[0]) || 
+        (computerChoice == Options[2] && playerChoice == Options[1])) 
+        {
+            computerScore++
+            pointerCMarker[computerScore].innerText = '✅'
+            pointerRoundDesc.innerText = 'COMPUTER WINS THE ROUND'
+        }
+
+    if (computerChoice == playerChoice) 
+        {
+            pointerRoundDesc.innerText = "IT'S A TIE"
+        }
 }
 
-let playGame = () => {
-    for (i = 0; i < 5; i++) {
-        getComputerChoice()
-        playRound(PlayerChoice, ComputerChoice)
+let emojiComputer = (computerChoice) => {
+    if (computerChoice == Options[0]) {
+        pointerComputerChoice.innerText = '✊' 
+    }
+    if (computerChoice == Options[1]) {
+        pointerComputerChoice.innerText = '✋' 
+    }
+    if (computerChoice == Options[2]) {
+        pointerComputerChoice.innerText = '✌️' 
     }
 }
+// var //
+let Options = ["ROCK", "PAPER", "SCISSORS"];
+let playerOption;
+
+let playerScore = -1;
+let computerScore = -1;
+
+let playChoice
+let comChoice = ""
+// querys //
+
+
+const pointerRoundDesc = document.querySelector('.roundResult')
+
+const pointerComputerChoice = document.querySelector('.computerLastchoice')
+
+const pointerRock = document.querySelector('.rockButton');
+const pointerPaper = document.querySelector('.paperButton');
+const pointerScissors = document.querySelector('.scissorsButton');
+const pointerNum = document.querySelectorAll ('.marker')
+const pointerPlayerChoice = document.querySelector('.playerLastchoice')
+const pointerPMarker = document.querySelectorAll('.pmarker')
+const pointerCMarker = document.querySelectorAll('.cmarker')
+
+pointerRock.addEventListener('click', () => {
+    if (playerScore < 4 || computerScore < 4) {
+        playChoice = Options[0];
+        comChoice = getComputerChoice();
+        playRound (playChoice, comChoice);
+        emojiComputer(comChoice)
+        pointerPlayerChoice.innerText = '✊'       
+        
+    }
+    if (playerScore == 4 || computerScore == 4) {
+        pointerScissors.innerText = '🔁'
+        pointerRock.innerText = '🔁'
+        pointerPaper.innerText = '🔁'
+        pointerRock.addEventListener ('click', () => {
+            location.reload() 
+        })
+        pointerPaper.addEventListener ('click', () => {
+            location.reload() 
+        })
+        pointerScissors.addEventListener ('click', () => {
+            location.reload() 
+        })
+    }
+})
+
+pointerPaper.addEventListener('click', () => {
+    if (playerScore < 4 || computerScore < 4) {
+        playChoice = Options[1];
+        comChoice = getComputerChoice();
+        playRound (playChoice, comChoice);
+        emojiComputer(comChoice)
+        pointerPlayerChoice.innerText = '✋'
+    }
+    if (playerScore == 4 || computerScore == 4) {
+        pointerScissors.innerText = '🔁'
+        pointerRock.innerText = '🔁'
+        pointerPaper.innerText = '🔁'
+        pointerRock.addEventListener ('click', () => {
+            location.reload() 
+        })
+        pointerPaper.addEventListener ('click', () => {
+            location.reload() 
+        })
+        pointerScissors.addEventListener ('click', () => {
+            location.reload() 
+        })
+    }
+})
+
+pointerScissors.addEventListener('click', () => {
+    if (playerScore < 4 || computerScore < 4) {
+        playChoice = Options[2];
+        comChoice = getComputerChoice();
+        playRound (playChoice, comChoice);
+        emojiComputer(comChoice)
+        pointerPlayerChoice.innerText = '✌️'
+    }
+    if (playerScore == 4 || computerScore == 4) {
+        pointerScissors.innerText = '🔁'
+        pointerRock.innerText = '🔁'
+        pointerPaper.innerText = '🔁'
+        pointerRock.addEventListener ('click', () => {
+            location.reload() 
+        })
+        pointerPaper.addEventListener ('click', () => {
+            location.reload() 
+        })
+        pointerScissors.addEventListener ('click', () => {
+            location.reload() 
+        })
+    }
+})
